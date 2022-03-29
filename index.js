d13a0d12029a4d360557d8038b709118237e6150
yuran1811 <trieuvanbd123@gmail.com>
2022-03-29 18:41:21 +0700

    Update

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-const axios = require("axios");
-const fs = require("fs");
+import axios from 'axios';
+import { createReadStream, createWriteStream, writeFileSync } from 'fs';
 
 const getQuote = async () => {
-  try {
-    const { data } = await axios.get("https://quotes.rest/qod?language=en");
-    return data.contents.quotes[0];
-  } catch (err) {
-    console.error(err.message);
-    return {};
-  }
+	try {
+		const { data } = await axios.get('https://quotes.rest/qod?language=en');
+		return data.contents.quotes[0];
+	} catch (err) {
+		console.error(err.message);
+		return {};
+	}
 };
 
 (async () => {
-  const { quote, author } = await getQuote();
-  if (!quote) return;
-  fs.appendFile("README.md", `_**${quote}**_\n\n${author}`);
+	const { quote, author } = await getQuote();
+	if (!quote) return;
+
+	writeFileSync(
+		'README.md',
+		`_**${quote}**_\n\n${author}
+	
+`,
+		(err) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+		}
+	);
+
+	const src = createReadStream('info.md', {
+		flags: 'r',
+		encoding: 'utf8',
+	});
+	const dest = createWriteStream('README.md', {
+		flags: 'a+',
+		encoding: 'utf8',
+	});
+	src.pipe(dest);
 })();
